89bf5521b965d88c7bcfa303109928f665a696ac
Fernando <koynonia@gmail.com>
2018-04-06 10:56:50 -0300

    Clone Twitter
    Um estudo sobre PHP construindo um clone simples do Twitter.

---
+++ web/php/twitter_clone/db.class.php
@@ -0,0 +1,27 @@
+<?php
+
+class db {
+	
+	private $host = 'linoox.com.br';
+	private $usuario = 'linoox_admin';
+	private $senha = 'sh@l0N20';
+	private $database = 'linoox_twitter';
+	
+	public function conecta_mysql(){
+		
+		// Cria a conexão
+		$con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database);
+		
+		// Ajustar o charset de comunicação entre a aplicação e o banco de dados
+		mysqli_set_charset($con, 'utf8');
+		
+		// Verificar se houve erros de conexãor
+		if(mysqli_connect_errno()){
+			echo 'Erro ao tentar se conectar com o BD MySQL: \n\n'.mysqli_connect_error();
+		}
+		
+		return $con;
+	}
+}
+
+?>
---
+++ web/php/twitter_clone/validar_acesso.php
@@ -0,0 +1,39 @@
+<?php
+	
+		session_start();
+	
+		require_once('db.class.php');
+
+		$usuario = $_POST['usuario'];
+		$senha = $_POST['senha'];
+
+		$sql = "SELECT usuario, email FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
+
+		$objDb = new db();
+		$link = $objDb->conecta_mysql();
+	
+	// update = true/false
+	// insert = true/false
+	// select = false/resorce
+	// delete true/false
+	
+		$resultado_id = mysqli_query($link, $sql);
+	
+		if($resultado_id){
+			// Verifica se a conexão e a consulta tiveram êxito
+			$dados_usuario = mysqli_fetch_array($resultado_id);
+			// Verifica se retornou o registro
+			if(isset($dados_usuario['usuario'])){
+			
+					$_SESSION['usuario'] = $dados_usuario['usuario'];
+					$_SESSION['email'] = $dados_usuario['email'];
+			
+					header('Location: home.php');
+			} else {
+		 			header('Location: index.php?erro=1');
+			}
+		} else {
+			echo 'Erro na execução da consulta! Favor entrar com o administrador do site!';
+		}
+
+?>
