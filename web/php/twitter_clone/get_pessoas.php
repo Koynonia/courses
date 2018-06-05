d2e4de9e5a72ba4199e4dfca5267168215472712
Fernando <koynonia@gmail.com>
2018-06-05 20:16:59 -0300

    Procurado pessoas
    Aula 302

---
+++ web/php/twitter_clone/get_pessoas.php
@@ -0,0 +1,37 @@
+<?php
+
+	session_start();
+	
+	if(!$_SESSION['usuario']){
+		header('Location: index.php?erro=1');
+	}
+	
+	require_once('db.class.php');
+	
+	$nome_pessoa = $_POST['nome_pessoa'];
+	$id_usuario = $_SESSION['id_usuario'];
+	
+	// Instancia do objeto DB
+	$objDb = new db();
+
+	// Conexão com o BD
+	$link = $objDb->conecta_mysql();
+	
+	// Consulta ao BD
+	$sql = " SELECT * FROM usuarios ";
+	$sql.= " WHERE usuario LIKE '%$nome_pessoa%' AND id <> $id_usuario ";
+
+	// Execusão da consulta
+	$resultado_id = mysqli_query($link, $sql);
+
+	if($resultado_id){
+		
+		while($registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC)){
+			echo '<a href="#" class="list-group-item">';
+				echo '<strong>'.$registro['usuario'].'</strong> <small> - '.$registro['email'].' </small>';
+			echo '</a>';
+		}
+	} else {
+			echo 'Erro na consulta de usuarios no Banco de Dados!';
+	}
+?>
--- web/php/twitter_clone/procurar_pessoas.php
+++ web/php/twitter_clone/procurar_pessoas.php
@@ -23,37 +23,22 @@
 
 			$(document).ready(function(){
 
-				$('#btn_tweet').click(function(){
+				$('#btn_procurar_pessoa').click(function(){
 
-					if($('#texto_tweet').val().length > 0){
+					if($('#nome_pessoa').val().length > 0){
 
 							$.ajax({
-								url: 'inclui_tweet.php',
+								url: 'get_pessoas.php',
 								method: 'post',
-								// Em caso de formularios com varios campos, usa-se a função do jquery 'serialize'
-								// que retorna a propriedade 'name' como chaves dos campos e seus valores de uma tag 'form',
-								// e retornando um JSON, exemplo:
-								data: $('#form_tweet').serialize(),
-								// ou poderia ser campo a campo, usando a tag "div"
-								//data: { texto_tweet: $('#texto_tweet').val() },
+								data: $('#form_procurar_pessoas').serialize(),
 								success: function(data){
-									$('#texto_tweet').val('');
-									atualizaTweet();
+									$('#pessoas').html(data);
 								}
 							});
 					}
 
 				});
 				
-				function atualizaTweet(){
-					$.ajax({
-						url: 'get_tweet.php',
-						success: function(data){
-							$('#tweets').html(data);
-						}
-					});
-				}
-				atualizaTweet();
 			});
 
 		</script>
@@ -77,6 +62,7 @@
 
 				<div id="navbar" class="navbar-collapse collapse">
 					<ul class="nav navbar-nav navbar-right">
+						<li><a href="home.php">Home</a></li>
 						<li><a href="sair.php">Sair</a></li>
 					</ul>
 				</div><!--/.nav-collapse -->
@@ -102,23 +88,22 @@
 			<div class="col-sm-6 col-md-6">
 				<div class="panel panel-default">
 					<div class="panel-body">
-						<form id="form_tweet" class="input-group">
-							<input type="text" id="texto_tweet" name="texto_tweet" class="form-control" placeholder="O que está acontecendo agora?" maxlength="140" />
+						<form id="form_procurar_pessoas" class="input-group">
+							<input type="text" id="nome_pessoa" name="nome_pessoa" class="form-control" placeholder="Quem você está procurando?" maxlength="140" />
 							<span class="input-group-btn">
-								<button class="btn btn-default" id="btn_tweet" type="button">Tweet</button>
+								<button class="btn btn-default" id="btn_procurar_pessoa" type="button">Procurar</button>
 							</span>
 						</form>
 					</div>
 				</div>
 				
-				<div id="tweets" class="list-group">
+				<div id="pessoas" class="list-group">
 					
 				</div>
 			</div>
 			<div class="col-sm-3 col-md-3">
 				<div class="panel panel-default">
 					<div class="panel-body">
-						<h4><a href="procurar_pessoas.php">Procurar por pessoas</a></h4>
 					</div>
 				</div>
 			</div>
