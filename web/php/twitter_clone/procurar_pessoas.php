f1350f1db63fb1f0126d1dd3a75d99fa5edde87c
Fernando M. Oliveira <Koynonia@users.noreply.github.com>
2018-08-14 19:56:54 -0300

    Update procurar_pessoas.php
    Aula 305 - Deixando de seguir pessoas.

--- web/php/twitter_clone/procurar_pessoas.php
+++ web/php/twitter_clone/procurar_pessoas.php
@@ -33,58 +33,6 @@
 								data: $('#form_procurar_pessoas').serialize(),
 								success: function(data){
 									$('#pessoas').html(data);
-								}
-							});
-					}
-
-				});
-				
-			});
-
-		</script>
-
-	</head>
-
-	<body>
-
-		<!-- Static navbar -->
-		<nav class="navbar navbar-default navbar-static-top">
-			<div class="container">
-<?php
-	session_start();
-
-	if(!$_SESSION['usuario']){
-		header('Location: index.php?erro=1');
-	}
-?>
-
-<!DOCTYPE HTML>
-<html lang="pt-br">
-	<head>
-		<meta charset="UTF-8">
-
-		<title>Twitter clone</title>
-
-		<!-- jquery - link cdn -->
-		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-
-		<!-- bootstrap - link cdn -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
-		<script type="text/javascript">
-
-			$(document).ready(function(){
-
-				$('#btn_procurar_pessoa').click(function(){
-
-					if($('#nome_pessoa').val().length > 0){
-
-							$.ajax({
-								url: 'get_pessoas.php',
-								method: 'post',
-								data: $('#form_procurar_pessoas').serialize(),
-								success: function(data){
-									$('#pessoas').html(data);
 									
 									$('.btn_seguir').click( function(){
 										// A função seletora ($), com o "this" recupera o 
@@ -98,6 +46,20 @@
 											data: { seguir_id_usuario: id_usuario },
 											success: function(data){
 												alert('Registro efetuado com sucesso!');
+											}
+										});
+									});
+									
+									$('.btn_deixar_seguir').click( function(){
+										
+										var id_usuario = $(this).data('id_usuario');
+										
+										$.ajax({
+											url: 'deixar_seguir.php',
+											method: 'post',
+											data: { deixar_seguir_id_usuario: id_usuario },
+											success: function(data){
+												alert('Registro removido com sucesso!');
 											}
 										});
 									});
